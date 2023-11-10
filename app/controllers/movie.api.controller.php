b3e81851378f510ef9ff500fc615dcd0dd86eb76
Simon Aceto <xsub123@gmail.com>
2023-11-10 20:47:57 -0300

    Agregada funcion de POST

--- app/controllers/movie.api.controller.php
+++ app/controllers/movie.api.controller.php
@@ -22,10 +22,10 @@ require_once './app/models/movie.model.php';
             $movies = $this->model->getAllMovies();
             $this->view->response($movies, 200);
         } else {
-            $movie = $this->model->getMovieById($params[':ID']);  //Sino, traigo solo la tarea cuyo ID solicitaron
+            $movie = $this->model->getMovieById($params[':ID']);  //Sino, traigo solo la pelicula cuyo ID solicitaron
             
             if (empty($movie)){
-                $this->view->response('La tarea con el id='.$params[':ID'].' no existe.'  , 404);
+                $this->view->response('La pelicula con el id='.$params[':ID'].' no existe.'  , 404);
             }
             else{
                 $this->view->response($movie, 200); // Envio la pelicula (Sin subrecurso)
@@ -65,9 +65,77 @@ require_once './app/models/movie.model.php';
                 } else
                     $this->view->response($movie, 200); // Envio la pelicula (Sin subrecurso)
             } else {
-                $this->view->response('La tarea con el id='.$params[':ID'].' no existe.'  , 404);
+                $this->view->response('La pelicula con el id='.$params[':ID'].' no existe.'  , 404);
             }
         }
+    }
+
+
+    function create($params = []){
+
+        $body = $this->getData();
+
+
+
+        $titulo = $body->titulo;
+        $sinopsis = $body->sinopsis;
+        $director = $body->director;
+        $fecha = $body->año_lanzamiento;
+        $cast = $body->cast;
+        $plataforma = $body->plataforma_id;
+        $link_portada = $body->link_portada;
+
+
+        //Corregir esto
+        if (empty($titulo) || empty($sinopsis) || empty($director) || empty($fecha)
+        || empty($cast) || empty($plataforma) || empty($link_portada)) {
+            $this->view->response("Complete los datos", 400);
+        } else {
+            $id = $this->model->POSTmovie($titulo,$sinopsis,$director,$fecha,$cast,$plataforma,$link_portada);
+
+            // Devuelvo la pelicula creada
+            $pelicula = $this->model->getMovieById($id);
+            $this->view->response($pelicula, 201);
+        }
+
+
+    }
+
+    public function update ($params = []){
+        $id = $params[':ID'];
+        $pelicula = $this->model->getMovieById($id);
+
+
+        //Compruebo que la pelicula que estoy queriendo editar exista
+        if($pelicula) {
+            $body = $this->getData();    //Me traigo los datos nuevos
+
+            //Datos nuevos
+            $titulo = $body->titulo;
+            $sinopsis = $body->sinopsis;
+            $director = $body->director;
+            $fecha = $body->año_lanzamiento;
+            $cast = $body->cast;
+            $plataforma = $body->plataforma_id;
+            $link_portada = $body->link_portada;
+
+            if (empty($titulo) || empty($sinopsis) || empty($director) || empty($fecha)
+            || empty($cast) || empty($plataforma) || empty($link_portada)){
+                $this->view->response("Complete los datos", 400);
+            }
+            else{
+                 //Llamo a la funcion del update
+                $this->model->PUTmovie($id,$titulo,$sinopsis,$director,$fecha,$cast,$plataforma,$link_portada);
+
+                $this->view->response('La pelicula con id='.$id.' ha sido modificada.', 200);
+            }
+            
+
+           
+        } else {
+            $this->view->response('La pelicula con id='.$id.' no existe.', 404);
+        }
+
     }
 
 
